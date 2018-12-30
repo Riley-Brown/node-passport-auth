4305ef0b72e0f69c230b37534f56728c134f2216
Riley-Brown <riley@riley.gg>
2018-12-30 16:45:32 -0500

    Added dashboard route with working logout logic and flash messaging

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 /* ===== WELCOME ROOT ROUTE ===== */
 router.get('/', (req, res) => res.render('Welcome'))
 
+/* ===== DASHBOARD ROUTE ===== */
+router.get('/dashboard', (req, res) => res.render('Dashboard'))
 
 module.exports = router;
