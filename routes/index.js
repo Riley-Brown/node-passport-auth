185f42e8c1496aef08a030361ac86b1d5732820f
Riley-Brown <riley@riley.gg>
2018-12-30 16:53:37 -0500

    added middleware to protect dashboard route if not logged in

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const {
+  ensureAuthenticated
+} = require('../config/auth');
 
 /* ===== WELCOME ROOT ROUTE ===== */
 router.get('/', (req, res) => res.render('Welcome'))
 
 /* ===== DASHBOARD ROUTE ===== */
-router.get('/dashboard', (req, res) => res.render('Dashboard'))
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    name: req.user.name
+  }))
 
 module.exports = router;
